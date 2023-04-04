b5a992090e0b8f809bd4fe083344de0bd6c6846f
predeep kumar singroul <prasingroul4899@gmail.com>
2023-04-05 02:03:21 +0530

    Services

--- Rock_paper_Scissor/src/main/java/com/pradeep/Services/PlayerDetailsService.java
+++ Rock_paper_Scissor/src/main/java/com/pradeep/Services/PlayerDetailsService.java
@@ -1,10 +1,16 @@
 package com.pradeep.Services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.pradeep.Model.Player;
@@ -15,17 +21,20 @@ import com.pradeep.Repository.PlayerRepository;
 public class PlayerDetailsService implements UserDetailsService{
 	
 	
+	
 	@Autowired
     private PlayerRepository playerRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Player> optionalStudent = user.findByEmail(username);
-        if (optionalStudent.isPresent()){
-            Student s1 = optionalStudent.get();
+        Optional<Player> optionalPlayer = playerRepository.findByEmail(username);
+        if (optionalPlayer.isPresent()){
+            Player player = optionalPlayer.get();
             List<GrantedAuthority> authorities = new ArrayList<>();
+    
 
-            return new Player(s1.getEmail(),s1.getPassword(),authorities);
+            return new User(player.getEmail(),player.getPassword(),authorities);
+            
         }else throw new BadCredentialsException("User details not found with this userName :"+username);
     }
 
