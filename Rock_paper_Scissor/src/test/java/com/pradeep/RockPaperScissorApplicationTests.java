410ce7ca212dfa788d0cf77d9540e3ba3b751127
predeep kumar singroul <prasingroul4899@gmail.com>
2023-04-04 18:10:30 +0530

    Exceptions

---
+++ Rock_paper_Scissor/src/test/java/com/pradeep/RockPaperScissorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
