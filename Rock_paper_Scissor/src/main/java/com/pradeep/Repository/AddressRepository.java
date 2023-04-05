581020afc18dee3a0458fb781d74a326eec26229
predeep kumar singroul <prasingroul4899@gmail.com>
2023-04-05 06:47:07 +0530

    addresss repo

--- Rock_paper_Scissor/src/main/java/com/pradeep/Repository/AddressRepository.java
+++ Rock_paper_Scissor/src/main/java/com/pradeep/Repository/AddressRepository.java
@@ -1,9 +1,13 @@
 package com.pradeep.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pradeep.Model.Address;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Integer>{
 
+	//address repository
+	
 }
